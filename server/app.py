b50737c658520ebd2f590bd5168fbec70b138d2d
Marysah <Marysah2@gmail.com>
2026-01-13 14:58:53 +0300

    solved

--- server/app.py
+++ server/app.py
@@ -1,8 +1,49 @@
 #!/usr/bin/env python3
 
-from flask import Flask
 
-app = Flask(__name__)
+import os
 
-if __name__ == '__main__':
+from flask import Flask,request,current_app, g, make_response, redirect,abort,session
+
+app = Flask(name)
+
+@app.before_request
+def app_path():
+    g.path = os.path.abspath(os.getcwd())
+
+@app.route("/")
+def index():
+    host = request.headers.get("Host")
+    appname = current_app.name
+    response_body =  f"""<h1>The host for this page is {host}</h1>
+               <h2>The name of this application is {appname}</h2>
+               <h3>The path of this application on the user's device is {g.path}</h3>"""
+    status_code = 200
+    headers = {}
+    return make_response(response_body, status_code, headers)
+
+@app.route("/duolingo")
+def redirect():
+return redirect("/")
+@app.route("/<stage_name>")
+def abortget_name(stage_name):
+match = session.query("StageName").filter(StageName == stage_name)[0]
+if not match:
+abort(404)
+return make_response(f"<h1>{stage_name} is an existing stage name!<h1>")
+if name == 'main':
     app.run(port=5555, debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
